f68ae03018e20129ae948c02916bd560b3bfed52
Johnny <johnny.martens@protonmail.com>
2022-09-24 12:15:21 -0400

    updates

--- src/App.js
+++ src/App.js
@@ -5,15 +5,30 @@ import { BrowserRouter as Router, Route } from "react-router-dom";
 import Books from "./pages/Books";
 import { books } from "./data";
 import BookInfo from "./pages/BookInfo";
+import Cart from "./pages/cart";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [cart, setCart] = useState([]);
+
+  function addToCart(book) {
+    setCart([...cart, book])
+  }
+  useEffect(() => {
+    console.log(cart)
+  }, [cart])
+
   return (
     <Router>
       <div className="App">
         <Nav />
         <Route path="/" exact component={Home} />
-        <Route path="/books" render={() => <Books books={books} />} />
-        <Route path="/books/1" render={() => <BookInfo books={books} />} />
+        <Route path="/books" exact render={() => <Books books={books} />} />
+        <Route
+          path="/books/:id"
+          render={() => <BookInfo books={books} addToCart={addToCart} />}
+        />
+        <Route path="/cart" render={() => <Cart books={books} />} />
         <Footer />
       </div>
     </Router>
--- src/components/Landing.jsx
+++ src/components/Landing.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import UndrawBooks from '../assets/Undraw_Books.svg'
 
 
@@ -11,9 +12,9 @@ const Landing = () => {
                     <div className="header__description">
                         <h1>Canada's most awarded online library platform</h1>
                         <h2>Find your dream book with <span className="purple">Library</span></h2>
-                        <a href="#features">
+                        <Link to="#features">
                             <button className="btn">Browse Books</button>
-                        </a>
+                        </Link>
                     </div>
                     <figure className='header__img--wrapper'>
                         <img src={UndrawBooks} alt="" />
